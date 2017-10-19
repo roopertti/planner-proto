c3064bbc5f15751a0fa77dd589d1b0703d6bdfea
Robert Kuhlmann <robert.kuhlmann2@gmail.fi>
2017-10-20 00:52:06 +0300

    made tasklist a tiny bit more dynamic

--- components/taskLists/taskLists.php
+++ components/taskLists/taskLists.php
@@ -1,20 +1,61 @@
-<div class="container">
-  <div class="row">
-    <div class="col-md-4">
-      <div class="tasklist-header">
-        <h4>Random tasklist</h4>
+<?php
+$taskLists = array(
+  0 => array(
+    "title" => "Random taskilista",
+    "author" => "Robert Kuhlmann",
+    "created" => "20.10.2017",
+    "tasks" => array(
+      0 => array(
+        "timestamp" => "20.10.2017",
+        "content" => "tee jotakki",
+        "done" => false
+      ),
+      1 => array(
+        "timestamp" => "20.10.2017",
+        "content" => "tee jotakki muuta",
+        "done" => false
+      )
+    )
+  )
+);
+
+if($taskLists && count($taskLists) > 0) {
+  ob_start();
+  ?>
+  <div class="container">
+    <div class="row">
+    <?php
+    foreach($taskLists as $list) {
+      ?>
+      <div class="col-md-4">
+        <div class="tasklist-header">
+          <h4><?=$list['title']?></h4>
+        </div>
+        <div class="tacklist-body">
+          <table class="table">
+            <tbody>
+              <?php
+              foreach($list['tasks'] as $task) {
+                echo '<tr>';
+                $taskContent = '<td>' . $task['timestamp'] . '</td>';
+                $taskContent .= '<td>' . $task['content'] . '</td>';
+                if(!$task['done']) {
+                  $taskContent .= '<td><button class="btn btn-primary"></button></td>';
+                } else {
+                  $taskContent .= "<td><button class='btn btn-success'><i class='fa fa-check' aria-hidden='true'></i></button></td>";
+                }
+                echo $taskContent;
+                echo '</tr>';
+              } ?>
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div class="tasklist-body">
-        <table class="table">
-          <tbody>
-            <tr>
-              <td>Timestamp</td>
-              <td>Do something</td>
-              <td><button class="btn btn-success"><i class="fa fa-check" aria-hidden="true"></i></button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <?php
+    } ?>
     </div>
-  </div>
-</div>
+  </div> <?php
+
+  $html = ob_get_clean();
+  echo $html;
+} ?>
