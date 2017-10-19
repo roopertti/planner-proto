1f66209aa1896a2b8e8b5cb9635f19403b9adf97
Robert Kuhlmann <robert.kuhlmann2@gmail.fi>
2017-10-20 00:12:45 +0300

    added a page header

---
+++ components/header/header.php
@@ -0,0 +1,15 @@
+<style>
+  .header-background {
+    text-align: center;
+    background-color: #ccc;
+    padding: 40px 0;
+    margin-bottom: 30px;
+  }
+
+  .planner-header {
+    color: white;
+  }
+</style>
+<div class="header-background container-fluid">
+  <h1 class="planner-header">Planner</h1>
+</div>
--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
 
 $path = './components/';
-
+require_once($path.'header/header.php');
 require_once($path.'taskLists/taskLists.php');
 
 ?>
