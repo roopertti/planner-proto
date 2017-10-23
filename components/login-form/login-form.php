9344199a9790624660e507dc5914225b87e627d1
Robert Kuhlmann <robert.kuhlmann2@gmail.fi>
2017-10-24 01:28:34 +0300

    Registeration started + front-end on register form

--- components/login-form/login-form.php
+++ components/login-form/login-form.php
@@ -1,15 +1,53 @@
 <div class="container">
-  <form action="./" method="post">
-    <div class="form-group">
-      <label for="username">Username</label>
-      <input type="text" name="username" class="form-control">
+  <div class="row">
+    <div class="col">
+      <form action="./" method="post" id="loginForm">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" name="username" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="text" name="password" class="form-control">
+        </div>
+        <div class="form-group">
+          <input type="submit" name="login" class="btn btn-primary">
+        </div>
+      </form>
     </div>
-    <div class="form-group">
-      <label for="password">Password</label>
-      <input type="text" name="password" class="form-control">
+    <div class="col">
+      <form action="./" method="post" id="registerForm" style="display: none;">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" name="username" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="text" name="password" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="password">Password again</label>
+          <input type="text" name="password_again" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="password">Email</label>
+          <input type="text" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+          <input type="submit" name="register" class="btn btn-primary">
+        </div>
+      </form>
+      <button class="btn btn-success" id="formToggleBtn">Register</button>
     </div>
-    <div class="form-group">
-      <input type="submit" name="login" class="btn btn-primary">
-    </div>
-  </form>
+  </div>
 </div>
+<script type="text/javascript">
+  $(function() {
+    $(document).on('click', '#formToggleBtn', function() {
+      console.log('clicked');
+      $('#registerForm').animate({
+        height: 'toggle'
+      });
+    });
+  });
+</script>
--- routes.php
+++ routes.php
@@ -1,6 +1,7 @@
 <?php
 
 $path = './components/';
+require_once($path.'header/header.php');
 
 if(isset($_POST['login'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
@@ -11,14 +12,41 @@ if(isset($_POST['login'])) {
   if(mysqli_num_rows($result) == 1) {
     $_SESSION['logged_user'] = $username;
   } else {
-    require_once($path.'header/header.php');
     $alert = printAlert("Login failed.", true);
     echo $alert;
     require_once($path.'login-form/login-form.php');
+    die();
   }
-} else {
-  require_once($path.'header/header.php');
-  require_once($path.'login-form/login-form.php');
+}
+
+if(isset($_POST['register'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $password_again = mysqli_real_escape_string($db, $_POST['password_again']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+
+  if($password != $password_again) {
+    echo printAlert("Passwords didn't match!", true);
+    require_once($path.'login-form/login-form.php');
+    die();
+  } else {
+    $sql = "SELECT id FROM user WHERE username = '$username'";
+    $result = mysqli_query($db, $sql);
+    if(mysqli_num_rows($result) > 0) {
+      echo printAlert("Username has already been taken!", true);
+      require_once($path.'login-form/login-form.php');
+    } else {
+      $sql = "INSERT INTO user(username, email, password) VALUES('$username','$email','$password')";
+      $result = mysqli_query($db, $sql);
+      if(!$result) {
+        echo printAlert("User creation failed!", true);
+        require_once($path.'login-form/login-form.php');
+      } else {
+        echo printAlert("User succesfully created!", false);
+        require_once($path.'login-form/login-form.php');
+      }
+    }
+  }
 }
 
 if(isset($_SESSION['logged_id']) && !empty($_SESSION['logged_id'])) {
@@ -28,7 +56,6 @@ if(isset($_SESSION['logged_id']) && !empty($_SESSION['logged_id'])) {
 }
 
 
-# require_once($path.'header/header.php');
-# require_once($path.'taskLists/taskLists.php');
+require_once($path.'login-form/login-form.php');
 
 ?>
