90ae14d8e514708ad98c6242d6c91a611f36e295
Robert Kuhlmann <robert.kuhlmann2@gmail.fi>
2017-11-06 00:29:55 +0200

    pikkumuutos functions.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php session_start(); ?>
 
-<?php # require_once('./config/db.php'); ?>
+<?php require_once('./config/db.php'); ?>
 <?php require_once('./html-header.php'); ?>
 <?php require_once('./functions.php'); ?>
 <?php require_once('./routes.php'); ?>
